67ebc93659c80b906519b0213386cf9b62ebb2b2
paderinandrey <andy.paderin@gmail.com>
2022-01-29 15:26:00 +0300

    Move some fields from Tasks to Conversion

--- db/migrations/000004_create_tasks_table.up.sql
+++ db/migrations/000004_create_tasks_table.up.sql
@@ -3,15 +3,7 @@ CREATE TABLE tasks (
   project_id INTEGER REFERENCES projects,
   task_type task_type NOT NULL,
   status status NOT NULL DEFAULT 'queued',
-  download_time INTEGER,
-  upload_time INTEGER,
-  file_size INTEGER,
-  file_path TEXT,
-  result_path TEXT,
-  time_spent INTEGER,
   url TEXT,
-  error TEXT,
   created_at TIMESTAMP WITH TIME ZONE NOT NULL,
-  updated_at TIMESTAMP WITH TIME ZONE,
-  deleted_at TIMESTAMP WITH TIME ZONE
+  updated_at TIMESTAMP WITH TIME ZONE
 );
